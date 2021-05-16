79100424fbb6e64ed2c955a3df4b6f72da24b9e6
Jessica <simply.me05@hotmail.com>
2021-05-16 12:03:43 -0400

    updated workout.js file

--- Develop/models/workout.js
+++ Develop/models/workout.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-  name: {
-    type: String,
-    unique: true,
+  day: {
+    type: Date,
+    default: Date.now,
   },
   exercises: [
     {
@@ -38,6 +38,6 @@ const WorkoutSchema = new Schema({
   ],
 });
 
-const Workout = mongoose.model("Workout", WorkoutSchema);
+const Workout = mongoose.model("workout", WorkoutSchema);
 
 module.exports = Workout;
