678879586dbf6680852fed15d28274a58fa45965
Jessica <simply.me05@hotmail.com>
2021-05-16 11:53:52 -0400

    fixed routes error needed exports added

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,49 +1,51 @@
 const db = require("../models");
 
-app.get("/workouts", (req, res) => {
-  db.Workout.find({})
-    .then((dbWorkout) => {
-      res.json(dbWorkout);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
-});
+module.exports = (app) => {
+  app.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
 
-app.get("api/workouts/stats", (req, res) => {
-  db.Workout.find({})
-    .then((dbWorkout) => {
-      res.json(dbWorkout);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
-});
+  app.get("/api/workouts/stats", (req, res) => {
+    db.Workout.find({})
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
 
-app.post("/api/workouts", ({ body }, res) => {
-  db.Workout.create(body)
-    .then((dbWorkout) => {
-      res.json(dbWorkout);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
-});
+  app.post("/api/workouts", ({ body }, res) => {
+    db.Workout.create(body)
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
 
-app.put("api/workouts/:id", (req, res) => {
-  db.Workout.find({})
-    .then(({ _id }) =>
-      db.Workout.findOneAndUpdate(
-        {},
-        { $push: { exercise: _id } },
-        { new: true }
+  app.put("api/workouts/:id", (req, res) => {
+    db.Workout.find({})
+      .then(({ _id }) =>
+        db.Workout.findOneAndUpdate(
+          {},
+          { $push: { exercise: _id } },
+          { new: true }
+        )
       )
-    )
-    .populate("exercise")
-    .then((dbWorkout) => {
-      res.json(dbWorkout);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
-});
+      .populate("exercise")
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+};
